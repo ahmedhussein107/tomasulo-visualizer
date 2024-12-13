a79476cf00e9404cfd306606e2bd4ca26d9b4246
Ahmed Hussein <ahmedhusseinali107@gmail.com>
2024-12-13 15:20:55 +0200

    strated working on parsing input

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
+    const [index, setIndex] = useState(0);
+    const [views, setViews] = useState([]);
+
+    if (isLoading) return <h1>Loading ...</h1>;
 
     return (
-        <>
-            <h1>Hello</h1>
-        </>
+        <div>
+            <h1>{views[index]}</h1>
+            <button onClick={() => setIndex((prev) => (prev + 1) % views.length)}>
+                Next
+            </button>
+        </div>
     );
 }
 
